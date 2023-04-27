221df58bb07f630c8fb839c4306d73d39b62544f
Marta Chaika <janeseagul@gmail.com>
2023-04-27 11:41:53 +0300

    First commit

---
+++ Algorithms I/BinarySearch.py
@@ -0,0 +1,26 @@
+"""Given an array of integers nums which is sorted in ascending order, and an
+integer target, write a function to search target in nums.If target exists,
+then return its index. Otherwise, return -1.
+You must write analgorithm with O(log n) runtime complexity.
+Example1:
+Input: nums = [-1, 0, 3, 5, 9, 12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+Input: nums = [-1, 0, 3, 5, 9, 12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1 """
+
+
+def search(self, nums: List[int], target: int) -> int:
+    first = 0
+    last = len(nums) - 1
+    while first <= last:
+        mid = (first + last) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            last = mid - 1
+        else:
+            first = mid + 1
+    return -1
